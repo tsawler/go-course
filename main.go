f1a48fd680286719885d67f411311d0c19031920
Trevor Sawler <trevor.sawler@gmail.com>
2020-10-12 15:28:14 -0300

    Lecture 4

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
 // Home handles requests to home page
 func Home(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprintf(w, "Hello, world!")
@@ -18,7 +20,9 @@ func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", AboutPageHandler)
 	http.HandleFunc("/contact", ContactPageHandler)
-	err := http.ListenAndServe(":8080", nil)
+
+	log.Println(fmt.Sprintf("Starting web server on port %s", portNumber))
+	err := http.ListenAndServe(portNumber, nil)
 	if err != nil {
 		log.Fatal("Error Starting the HTTP Server : ", err)
 		return
