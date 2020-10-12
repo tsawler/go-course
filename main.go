dac9294e858d4da9621582ac96a0a85221fa90fd
Trevor Sawler <trevor.sawler@gmail.com>
2020-10-12 15:03:18 -0300

    Lecture 2

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
+// main starts a simple web server on port 8080, and writes hello world to the browser in two languages.
 func main() {
-	fmt.Println("Hello, world!")
-	fmt.Println("こんにちは世界")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprint(w, "Hello, world! こんにちは世界")
+	})
+
+	_ = http.ListenAndServe(":8080", nil)
 }
